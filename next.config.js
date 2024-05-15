95113bff436d26e2b0d615411b14d4adae803e7b
defifofum <defifofum0@gmail.com>
2024-05-15 17:42:11 -0500

    refactor: replace yarn with npm

--- next.config.js
+++ next.config.js
@@ -4,23 +4,22 @@ const withNextra = require('nextra')({
   themeConfig: './theme.config.jsx',
 })
 
-
+const defaultRepositoryName = 'defifofum.github.io';
 // Add your custom Next.js config here
 let config = {
   output: 'export',
   images: {
     unoptimized: true,
   },
+  assetPrefix: `/${defaultRepositoryName}/`,
+  basePath: `/${defaultRepositoryName}`,
 }
 
 if (process.env.GITHUB_REPOSITORY) {
   const repositoryName = process.env.GITHUB_REPOSITORY.split('/')[1]
 
-  config = {
-    ...config,
-    assetPrefix: `/${repositoryName}/`,
-    basePath: `/${repositoryName}`,
-  }
+  config.assetPrefix =`/${repositoryName}/`,
+  config.basePath = `/${repositoryName}`
 }
 
 module.exports = withNextra(config)
